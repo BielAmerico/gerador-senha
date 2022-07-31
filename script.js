c43283108b879e02c3ce343461e742cd179a80bf
Gabriel Americo <bielgan@hotmail.com>
2022-07-31 17:02:05 -0300

    Ajustando a funcao de copiar a senha

--- script.js
+++ script.js
@@ -8,28 +8,25 @@ let password = document.querySelector("#password");
 let containerPassword = document.querySelector("#container-password");
 
 let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@!#."
-let novaSenha = "";
 
 sizePassword.innerHTML = sliderElement.ariaValueMax;
 
-sliderElement.oninput = function(){
+sliderElement.oninput = function() {
     sizePassword.innerHTML = this.value;
 }
 
-function gerarPassword(){
+function gerarPassword() {
 
-    let pass= "";
+    let pass = "";
     for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
-
         pass += charset.charAt(Math.floor(Math.random() * n));
     }
     
     containerPassword.classList.remove("hide");
     password.innerHTML = pass;
-   
 }
 
-function copyPassword(){
+function copyPassword() {
+    navigator.clipboard.writeText(password.innerHTML);
     alert("Senha copiada com sucesso")
-    navigator.clipboard.writeText(novaSenha);
 }
